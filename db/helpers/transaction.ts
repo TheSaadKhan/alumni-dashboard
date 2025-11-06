316eb188ecfcfc73cc63f5344ca8e6f0cdc75fe6
TheSaadKhan <saadkhanofficialwork@gmail.com>
2025-11-07 00:18:19 +0530

    mino fix

--- db/helpers/transaction.ts
+++ db/helpers/transaction.ts
@@ -1,5 +1,4 @@
-﻿import { supabaseAdmin } from '../client/supabase-server';
-
+﻿
 // Supabase doesn't support multi-statement transactions over the REST SQL API easily.
 // If using pg client or Edge runtime, implement transactions there.
 // Placeholder wrapper for consistent API.
